355f3a8edfcee2c11253f80f830dcd24f6ecc2ca
odedmasala <odedmasala6@gmail.com>
2022-11-18 17:56:04 +0200

    start build app

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "/app.css"
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+import reactDOM from "react-dom/client";
+import "/index.css"
+import App from "./App";
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <react.StrictMode>
+    <App />
+  </react.StrictMode>
+);
