de850daece6533af950efa142507b47462aadaee
odedmasala <odedmasala6@gmail.com>
2022-11-18 18:21:14 +0200

    init react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "/app.css"
+import "./app.css"
 const App = () => {
   return (
     <div>App</div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import react from "react";
 import reactDOM from "react-dom/client";
-import "/index.css"
+import "./index.css"
 import App from "./App";
 
 const root = reactDOM.createRoot(document.getElementById("root"));
