ca370af333fa63cb34b3fee6025bb1d9b0140222
odedmasala <odedmasala6@gmail.com>
2022-11-28 00:16:27 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,10 @@ const App = () => {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={isAuth ? <HomePage /> : <LoginPage />} />
+            <Route
+              path="/"
+              element={isAuth ? <Navigate to="/home" /> : <LoginPage />}
+            />
             <Route
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
